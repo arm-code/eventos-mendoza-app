2ad0bcb942304a3d38938def9728ecc29d4ac96a
Alexis Romero Mendoza <alexis.rinnegan@gmail.com>
2026-09-22 20:13:10 -0600

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,14 +7,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
     return (
       <input
         type={type}
-        onFocus={(e) => {
-          e.target.select()
-          onFocus?.(e)
-        }}
-        onClick={(e) => {
-          ;(e.target as HTMLInputElement).select()
-          onClick?.(e)
-        }}
+
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           type === "number" && "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
